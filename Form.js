d1ec373f53fab637a4b2066c2c89cb08c50f3924
sultana-teach4 <73976914+sultana-teach4@users.noreply.github.com>
2021-08-26 14:47:29 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,42 @@
+class Form {
+  constructor() {
+   this.input = createInput("Name") 
+   this.button = createButton("Play")
+   this.greeting = createElement("h2")
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+    
+    //var input = createInput("Name");
+    //var button = createButton('Play');
+    
+    this.input.position(130, 160)
+    this.button.position(250, 200)
+
+    this.button.mousePressed(()=>{
+     // alert("in button pressed")
+      this.input.hide();
+     this.button.hide();
+
+      player.name = this.input.value();
+      
+      playerCount+=1;
+      player.index=playerCount;
+      player.update();
+      player.updateCount(playerCount);
+     // var greeting = createElement('h3');
+      this.greeting.html("Hello " + player.name )
+      this.greeting.position(130, 160)
+    });
+
+  }
+
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+}
